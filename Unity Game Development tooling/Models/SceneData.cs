21cf52453ce1be411d3fc9029e204804e1aca5b4
Gekonisko <jakubocytko@student.agh.edu.pl>
2025-10-19 16:28:29 +0200

    init commit

---
+++ Unity Game Development tooling/Models/SceneData.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Unity_Game_Development_tooling.Models
+{
+    public class SceneData
+    {
+        public string SceneName { get; set; }
+        public List<GameObjectData> RootObjects { get; set; } = new();
+    }
+}
